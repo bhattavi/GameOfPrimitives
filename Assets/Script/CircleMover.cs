b3613a1fbcfb8aa937a3bd414765dfb46f46b4a4
abhinav <bhattabhinav@github.com>
2020-03-22 12:58:05 -0400

    initial commit

---
+++ Assets/Script/CircleMover.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMover : MonoBehaviour
+{
+    public float speed = 15;
+
+    int degree;
+    
+    //float x, y, z;
+
+    void Start()
+    {
+        degree = Random.Range(0, 180);
+        // degree = Random.Range(0, 180);
+        transform.Rotate(0, 0, degree);
+
+    }
+
+    void Update()
+
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -6, 6), Mathf.Clamp(transform.position.y, -4, 4), 0);
+       
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+        degree = Random.Range(0, 180);
+
+
+        if (transform.localPosition.x <= -6)
+        {
+            transform.Rotate(0, 0, degree);
+        }
+
+        if (6 <= transform.localPosition.x)
+        {
+            transform.Rotate(0, 0, degree);
+        }
+
+        if (transform.localPosition.y <= -4)
+        {
+            // Debug.Log("I am below the camera's view.");
+
+            transform.Rotate(0, 0, degree);
+
+
+        }
+
+        if (4 <= transform.localPosition.y)
+        {
+            // Debug.Log("I am above the camera's view.");
+
+            transform.Rotate(0, 0, degree);
+
+
+        }
+
+    }
+
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool isSquaredDestoryed = false;
+    bool isCircleDestoryed = false;
+    public string nextScene;
+
+   public Text score;
+   static int total = 0;
+   public int clicks = 5;
+   public Text remClicks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score.text = total.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        //}
+      
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+      
+
+
+        if (isCircleDestoryed && isSquaredDestoryed)
+        {
+            SceneManager.LoadScene(nextScene);
+           
+        }
+        else
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+
+
+
+                if (hit.collider != null)
+                {
+                    if (hit.collider.gameObject.CompareTag("Square"))
+                    {
+
+                        Destroy(hit.collider.gameObject);
+                        isSquaredDestoryed = true;
+                        
+                        total += 1;
+                        score.text = total.ToString();
+                       
+
+
+                    }
+                    if (hit.collider.gameObject.CompareTag("Circle"))
+                    {
+
+
+                        Destroy(hit.collider.gameObject);
+                        isCircleDestoryed = true;
+                       
+                        total += 5;
+                        score.text = total.ToString();
+
+
+                    }
+
+                }
+                else
+                {
+                    clicks--;
+                    remClicks.text = clicks.ToString();
+                    if (clicks <= 0)
+                    {
+                        SceneManager.LoadScene(nextScene);
+                    }
+                }
+            }
+        }
+      
+    }
+}
